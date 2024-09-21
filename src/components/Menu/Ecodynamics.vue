2df0fb03c6093b99b4fd2dded8cd31c018953983
Hannahuy <163801394+Hannahuy@users.noreply.github.com>
2024-09-21 11:16:48 +0800

    互换

--- src/components/Menu/Ecodynamics.vue
+++ src/components/Menu/Ecodynamics.vue
@@ -1,11 +1,314 @@
 <template>
-    
+    <div class="page">
+        <div class="left-button">
+            <FunctionMenu :functionData="layerFunction" :Multiple="true" />
+        </div>
+        <div class="bottombox-button">
+            <el-button type="primary" class="bottombox-play" :class="{ active: activePlay === 'play' }"
+                @click="togglePlay"></el-button>
+        </div>
+        <div class="bottombox">
+            <div class="bottombox-slider">
+                <div :style="adjustedStyle">
+                    <span class="bottombox-slider-span">{{ formattedTime }}</span>
+                </div>
+                <el-slider :step="3600000" v-model="timePlay" :show-tooltip="false" :min="min" :max="max" :marks="marks"
+                    style="position: relative; z-index: 1; width: 150vh" @change="gettimePlay">
+                </el-slider>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { ref, computed, watch, onMounted, onBeforeUnmount } from "vue";
+import FunctionMenu from '../Common/FunctionMenu.vue'
+import dayjs from 'dayjs'
+import { callUIInteraction, addResponseEventListener } from "../../module/webrtcVideo/webrtcVideo.js";
 
+let layerFunction = [
+    {
+        name: "水位",
+        check: true,
+        image: new URL(
+            "../../assets/img/水位.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/水位-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "流速",
+        check: true,
+        image: new URL(
+            "../../assets/img/流速.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/流速-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "浮游动物碳",
+        check: true,
+        image: new URL(
+            "../../assets/img/浮游动物碳.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/浮游动物碳-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "浮游植物碳",
+        check: true,
+        image: new URL(
+            "../../assets/img/浮游植物碳.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/浮游植物碳-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "浮游植物磷",
+        check: false,
+        image: new URL(
+            "../../assets/img/浮游植物磷.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/浮游植物磷-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "浮游植物氮",
+        check: false,
+        image: new URL(
+            "../../assets/img/浮游植物氮.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/浮游植物氮-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "叶绿素",
+        check: false,
+        image: new URL(
+            "../../assets/img/叶绿素.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/叶绿素-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "初级生产力",
+        check: false,
+        image: new URL(
+            "../../assets/img/初级生产力.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/初级生产力-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "无机磷",
+        check: false,
+        image: new URL(
+            "../../assets/img/无机磷.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/无机磷-active.png",
+            import.meta.url
+        ).href,
+    },
+    {
+        name: "无机氮",
+        check: false,
+        image: new URL(
+            "../../assets/img/无机氮.png",
+            import.meta.url
+        ).href,
+        imageActive: new URL(
+            "../../assets/img/无机氮-active.png",
+            import.meta.url
+        ).href,
+    }
+]
+const timePlay = ref(dayjs().minute(0).second(0).valueOf());
+const activePlay = ref("");
+// 暂停/播放
+let previousPlayState = "";
+let intervalTime = null;
+let playInterval = null;
+const togglePlay = () => {
+    intervalTime = 16.6665;
+    previousPlayState = activePlay.value;
+    activePlay.value = activePlay.value === "play" ? "" : "play";
+    if (activePlay.value === "play") {
+        playInterval = setInterval(() => {
+            timePlay.value = dayjs(timePlay.value).add(1, "minute").valueOf();
+            if (activePlay.value !== "play") {
+                clearInterval(playInterval);
+            }
+        }, intervalTime);
+    } else {
+        clearInterval(playInterval);
+    }
+};
+const min = ref(dayjs().startOf("day").valueOf());
+// 将 max 设置为当天的23点
+const max = ref(dayjs().hour(23).minute(0).second(0).valueOf());
+
+const formattedTime = computed(() => {
+    const time = dayjs(timePlay.value);
+    return time.format("YYYY/MM/DD HH:mm");
+});
+
+const style = computed(() => {
+    const length = max.value - min.value,
+        progress = timePlay.value - min.value,
+        left = (progress / length) * 92;
+    return {
+        paddingLeft: `${left}%`,
+    };
+});
+
+const adjustedStyle = computed(() => {
+    const baseStyle = style.value;
+    const divWidth = 125; // 计算宽度为125px
+    const totalWidth = 1600;
+    const leftPadding = parseFloat(baseStyle.paddingLeft);
+
+    if ((leftPadding / 100) * totalWidth + divWidth > totalWidth) {
+        return {
+            paddingLeft: `${((totalWidth - divWidth) / totalWidth) * 100}%`,
+        };
+    }
+    return baseStyle;
+});
+
+// 定义 slider 的刻度
+const marks = computed(() => {
+    const marks = {};
+    const start = dayjs(min.value);
+    for (let i = 0; i <= 23; i++) {
+        const markTime = start.add(i, 'hour');
+        marks[markTime.valueOf()] = {
+            style: {
+                color: '#ffffff'
+            },
+            label: markTime.format('HH:mm')
+        };
+    }
+    return marks;
+});
+
+watch(timePlay, (newVal) => {
+    const currentTime = dayjs(newVal);
+    if (currentTime.minute() === 0 && currentTime.second() === 0) {
+
+    }
+    if (currentTime.isSame(dayjs(max.value))) {
+        activePlay.value = '';
+    }
+});
+
+// 监听时间轴
+const gettimePlay = (e) => {
+    timePlay.value = dayjs(e).second(0).valueOf(); // 确保秒数为 0
+    if (activePlay.value === "play") {
+        activePlay.value = "";
+    }
+}
 </script>
 
 <style scoped>
+.left-button {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    position: absolute;
+}
 
+.bottombox {
+    width: 100%;
+    padding: 0 3vh 0 10vh;
+    position: absolute;
+    bottom: 2.5vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.bottombox-button {
+    position: absolute;
+    bottom: 2.2vh;
+    left: 1.5%;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+}
+
+.bottombox-play {
+    background-image: url("../../assets/img/Timeout.png");
+    background-repeat: no-repeat;
+    background-color: #42aeff;
+    background-position: 55% 50%;
+    border-radius: 100%;
+    border: 0;
+    width: 4vh;
+    height: 4vh;
+}
+
+.bottombox-play.active {
+    background-image: url("../../assets/img/Play.png");
+    background-repeat: no-repeat;
+    background-color: #42aeff;
+    background-position: center;
+    background-size: 70% 70%;
+    border-radius: 100%;
+    border: 0;
+    width: 4vh;
+    height: 4vh;
+}
+
+.bottombox-slider-span {
+    width: 12.5vh;
+    background-color: #42aeff;
+    border-radius: 1.25rem;
+    color: white;
+    display: block;
+    text-align: center;
+    font-size: 1.4vh;
+}
+
+.bottombox-slider :deep(.el-slider__button) {
+    background-color: transparent;
+    border: 0;
+}
+
+.bottombox-slider :deep(.el-slider__bar) {
+    background: linear-gradient(to right, #0088ff, #00f2fe);
+    /* 渐变蓝色 */
+}
+
+.bottombox-slider :deep(.el-slider__marks-text) {
+    color: white !important;
+}
 </style>
